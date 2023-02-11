07bfa275314df1d6076984459f11328f273bcfae
Andrew Dobson <tandrewlopez@gmail.com>
2023-02-11 12:14:29 -0500

    transitioning to typescript

---
+++ leetcode/romanToInteger.js
@@ -0,0 +1,15 @@
+// * SYMBOL | VALUE
+// * I      | 1
+// * V      | 5
+// * X      | 10
+// * L      | 50
+// * C      | 100
+// * D      | 500
+// * M      | 1000
+
+// ? Given a roman numeral, covert it to an integer
+
+// * II => 2
+// * XII => 12
+// * XXVII => 27
+// * IV => 4
